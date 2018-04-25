336c818214458da6dc249ce0ad9c5b21d7e18899
Fridolin Pokorny <fridolin@redhat.com>
2018-04-25 17:02:29 +0200

    Version 0.0.5

--- thoth/common/__init__.py
+++ thoth/common/__init__.py
@@ -1,7 +1,7 @@
 """Shared code across Thoth analyzers."""
 
 __name__ = 'thoth-common'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from .helpers import cwd
 from .helpers import datetime2datetime_str
