3010d9afa88fe75a5c102aba1193c768a52780fc
Fridolin Pokorny <fridolin@redhat.com>
2018-04-26 16:09:37 +0200

    Add missing import

--- thoth/common/__init__.py
+++ thoth/common/__init__.py
@@ -7,6 +7,6 @@ from .helpers import cwd
 from .helpers import datetime2datetime_str
 from .helpers import datetime_str2timestamp
 from .helpers import parse_datetime
+from .helpers import datetime_str_from_timestamp
 from .json import SafeJSONEncoder
 from .logging import init_logging
-
