6d5db9100738847ad1dac0d19dac9b6b499ae497
Fridolin Pokorny <fridolin@redhat.com>
2018-05-22 10:03:03 +0200

    Add support for rsyslog logging endpoint

--- thoth/common/logging.py
+++ thoth/common/logging.py
@@ -19,7 +19,28 @@
 import os
 import logging
 
+from rfc5424logging import Rfc5424SysLogHandler
+
+_RSYSLOG_HOST = os.getenv('RSYSLOG_HOST')
+_RSYSLOG_PORT = os.getenv('RSYSLOG_PORT')
 _LOGGING_CONF_START = 'THOTH_LOG_'
+
+
+def _init_log_levels(logging_configuration: dict) -> None:
+    """Initialize log level based on configuration or env variables."""
+    env_logging_conf = {
+        key: val for key, val in os.environ.items() if key.startswith(_LOGGING_CONF_START)
+    }
+
+    for logger, level in env_logging_conf.items():
+        logger = 'thoth.' + logger[len(_LOGGING_CONF_START):].lower().replace('__', '.')
+        level = getattr(logging, level)
+        logging.getLogger(logger).setLevel(level)
+
+    if logging_configuration is not None:
+        for logger, level in logging_configuration.items():
+            level = getattr(logging, level)
+            logging.getLogger(logger).setLevel(level)
 
 
 def init_logging(logging_configuration: dict=None) -> None:
@@ -52,16 +73,14 @@ def init_logging(logging_configuration: dict=None) -> None:
     thoth_root_logger = logging.getLogger('thoth')
     thoth_root_logger.setLevel(logging.INFO)
 
-    env_logging_conf = {
-        key: val for key, val in os.environ.items() if key.startswith(_LOGGING_CONF_START)
-    }
+    _init_log_levels(logging_configuration)
 
-    for logger, level in env_logging_conf.items():
-        logger = 'thoth.' + logger[len(_LOGGING_CONF_START):].lower().replace('__', '.')
-        level = getattr(logging, level)
-        logging.getLogger(logger).setLevel(level)
-
-    if logging_configuration is not None:
-        for logger, level in logging_configuration.items():
-            level = getattr(logging, level)
-            logging.getLogger(logger).setLevel(level)
+    if _RSYSLOG_HOST and _RSYSLOG_PORT:
+        thoth_root_logger.info(f"Setting up logging to rsyslog endpoint {_RSYSLOG_HOST}:{_RSYSLOG_PORT}")
+        syslog_handler = Rfc5424SysLogHandler(address=(_RSYSLOG_HOST, _RSYSLOG_PORT))
+        thoth_root_logger.addHandler(syslog_handler)
+    elif int(bool(_RSYSLOG_PORT)) + int(bool(_RSYSLOG_HOST)) == 1:
+        raise RuntimeError(f"Please provide both RSYSLOG_HOST and RSYSLOG_PORT configuration"
+                           f"in order to use rsyslog logging, host: {_RSYSLOG_HOST}, port: {_RSYSLOG_PORT}")
+    else:
+        thoth_root_logger.info("Logging to rsyslog endpoint is turned off")
