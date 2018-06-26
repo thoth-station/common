70b1abaf11f835a3cdac8f4b2e97b8d36b6e6d98
Christoph Görn <goern@redhat.com>
2018-06-26 11:26:47 +0200

    releasing 0.0.8
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/common/__init__.py
+++ thoth/common/__init__.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+# thoth-common
+# Copyright(C) 2018 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """Shared code across Thoth analyzers."""
 
 from .helpers import cwd
@@ -10,4 +27,4 @@ from .logging import init_logging
 from .logging import logger_setup
 
 __name__ = 'thoth-common'
-__version__ = '0.0.8-rc.1'
+__version__ = '0.0.8'
--- thoth/common/helpers.py
+++ thoth/common/helpers.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """Various utilities to make your life easier."""
 
 import datetime
