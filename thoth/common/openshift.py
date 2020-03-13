c8d4a353250c438338f538f6a39ad0c06e2f962f
Francesco Murdaca <fmurdaca@redhat.com>
2020-03-13 11:58:06 +0100

    Adjust solver workflows ID
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/common/openshift.py
+++ thoth/common/openshift.py
@@ -800,9 +800,10 @@ class OpenShift:
                 labels={"component": "solver"},
             )
 
-        job_id = job_id or self.generate_id()
+        workflow_id = job_id or self.generate_id(solver)
+
         template_parameters = {
-            'THOTH_SOLVER_WORKFLOW_ID': job_id,
+            'THOTH_SOLVER_WORKFLOW_ID': workflow_id,
             'THOTH_SOLVER_NAME': solver,
             'THOTH_SOLVER_PACKAGES': packages.replace("\n", "\\n"),
             'THOTH_SOLVER_NO_TRANSITIVE': int(not transitive),
