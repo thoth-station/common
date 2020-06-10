c8f82b5c7c6ef5f79d90662c22653a754b803a55
Dominik Tuchyna <dtuchyna@redhat.com>
2020-06-10 15:11:03 +0200

    Remove repo name from workflow-id

--- thoth/common/openshift.py
+++ thoth/common/openshift.py
@@ -2239,7 +2239,7 @@ class OpenShift:
                 "No legacy implementation that would use workload operator, using Argo workflows.."
             )
 
-        workflow_id = f'{repository.replace("/","")}-{self.generate_id("srcopsmetrics-workflow")}'
+        workflow_id = self.generate_id("srcopsmetrics")
         template_parameters = {
             "WORKFLOW_ID": workflow_id,
             "REPOSITORY": repository,
