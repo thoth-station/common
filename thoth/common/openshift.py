74834510c06790d0f3f6e7291269d5d78d72a419
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-03-31 13:05:46 +0200

    Allow running authenticated provenance check

--- thoth/common/openshift.py
+++ thoth/common/openshift.py
@@ -1423,6 +1423,7 @@ class OpenShift:
         origin: Optional[str] = None,
         whitelisted_sources: Optional[List[str]] = None,
         debug: bool = False,
+        authenticated: bool = False,
         job_id: Optional[str] = None,
         kebechet_metadata: Optional[Dict[str, Any]] = None,
         justification: Optional[List[Dict[str, Any]]] = None,
@@ -1447,6 +1448,7 @@ class OpenShift:
                     },
                 }
             ),
+            "THOTH_AUTHENTICATED_PROVENANCE_CHECK": "1" if authenticated else "0",
             "THOTH_WHITELISTED_SOURCES": ",".join(whitelisted_sources or []),
             "THOTH_LOG_ADVISER": "DEBUG" if debug else "INFO",
             "THOTH_PROVENANCE_CHECKER_JOB_ID": job_id,
