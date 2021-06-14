ed38297d488960c5a0e5fdd98dd904f12b266f05
Kevin Postlethwait <k.postlethwait24@gmail.com>
2021-06-14 17:11:00 -0400

    add function argument to add value to template parameter for update advises

--- thoth/common/openshift.py
+++ thoth/common/openshift.py
@@ -1348,6 +1348,7 @@ class OpenShift:
         kebechet_metadata: Optional[Dict[str, Any]] = None,
         justification: Optional[List[Dict[str, Any]]] = None,
         stack_info: Optional[List[Dict[str, Any]]] = None,
+        package_update_info: Optional[Dict[str, str]] = None,
     ) -> Optional[str]:
         """Schedule an adviser run."""
         if not self.backend_namespace:
@@ -1403,6 +1404,11 @@ class OpenShift:
 
         if count is not None:
             template_parameters["THOTH_ADVISER_COUNT"] = str(count)
+
+        if package_update_info:
+            template_parameters["THOTH_ADVISER_DEPLOYMENT_PACKAGE_UPDATE"] = json.dumps(
+                package_update_info
+            )
 
         workflow_parameters = self._assign_workflow_parameters_for_ceph()
 
